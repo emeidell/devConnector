aa7efafc44b17bba52415566fc39feb65112c3bf
Evan <ejmeidell@gmail.com>
2018-05-14 21:16:50 -0600

    Added routes for API access

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route    GET api/profile/test
+// @Desc     Tests profile route
+// @access   Private
 router.get("/", (req, res) => res.json({
   msg: "Profile Works!"
 }));
