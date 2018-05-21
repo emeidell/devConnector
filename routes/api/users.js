b65a7df45b5837eb5692b67e51a7530bf18379f1
Evan <ejmeidell@gmail.com>
2018-05-20 19:53:36 -0600

    Successful login now signs and returns token

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../../models/Users");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // @route    GET api/users/test
 // @Desc     Tests users route
@@ -48,6 +50,55 @@ router.post("/register", (req, res) => {
           })
         })
       }
+    })
+})
+
+
+// @route    GET api/users/login
+// @Desc     Login user / returning JWT Token
+// @access   Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({
+      email
+    })
+    .then(user => {
+      if (!user) {
+        return (res.status(404).json({
+          email: "User not found"
+        }));
+      }
+
+      // Check Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            // User matched
+
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            } // Create JWT Payload
+
+            // Sign Token
+            jwt.sign(payload, keys.secretOrKey, {
+              expiresIn: 3600
+            }, (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              })
+            });
+          } else {
+            return (res.status(400).json({
+              password: "Password incorrect"
+            }));
+          }
+        })
     })
 })
 module.exports = router;
