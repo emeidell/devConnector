31dd66facf20892ba87bd0fad943045293d2f886
Evan <ejmeidell@gmail.com>
2018-05-23 00:12:27 -0600

    Added profile model and get request

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load models
+const profile = require("../../models/Profile");
+const users = require("../../models/Users");
 
 // @route    GET api/profile/test
 // @Desc     Tests profile route
 // @access   Private
-router.get("/", (req, res) => res.json({
+router.get("/test", (req, res) => res.json({
   msg: "Profile Works!"
 }));
+
+// @route    GET api/profile
+// @Desc     Get current users profile
+// @access   Private
+router.get("/", passport.authenticate("jwt", {session: false}), (req, res) => {
+  const errors = {};
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      if(!profile) {
+        errors.profile = "Their is no profile for this user";
+        return(res.status(404).json(errors))
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 module.exports = router;
